9e4d0b3d0298714d637f6d98fd7cf4f2ba197fe2
smallella <ella5898g5@gmail.com>
2023-10-06 15:34:04 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -5,14 +5,28 @@ module TsvBuddy
   # Converts a String with TSV data into internal data structure @data
   # arguments: tsv - a String in TSV format
   # returns: nothing
+  # def take_tsv(tsv); end
   def take_tsv(tsv)
+    @data = []
+    rows = tsv.strip.split("\n")
+    headers = rows[0].split("\t")
 
+    rows[1..].each do |row|
+      values = row.split("\t")
+      @data << Hash[headers.zip(values)]
+    end
   end
 
   # Converts @data into tsv string
   # arguments: none
   # returns: String in TSV format
+  #   def to_tsv; end
+  # end
   def to_tsv
+    return '' if @data.empty?
 
+    headers = @data[0].keys.join("\t")
+    rows = @data.map { |hash| hash.values.join("\t") }
+    "#{[headers, rows].join("\n")}\n"
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,14 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+# module YamlBuddy
+module YamlBuddy
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+end
